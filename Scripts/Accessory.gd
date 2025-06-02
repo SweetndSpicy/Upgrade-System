ec2c8dbf2573d4e916f9a448595fe447fb94f3d1
Onur Sarıyıldız <onursariyildiz97@gmail.com>
2025-06-02 20:49:39 +0300

    Add files via upload

---
+++ Scripts/Accessory.gd
@@ -0,0 +1,4 @@
+class_name Accessory extends Equipment
+
+@export var critical_chance :float
+@export var cooldown_rate :float
---
+++ Scripts/Equipment.gd
@@ -0,0 +1,7 @@
+class_name Equipment extends Resource
+
+@export var title: String
+@export var item_type: String
+@export var icon: Texture2D
+@export_multiline var description: String
+@export var enhancement_rate :int
